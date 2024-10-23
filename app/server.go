49fc10ca361946f32861dedf543d5654829dfa91
Andrew Rodriguez <andrewrod003@gmail.com>
2024-10-23 12:34:03 -0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
@@ -11,23 +13,38 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	//	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
-	// Return HTTP response
-	response := "HTTP/1.1 200 OK\r\n\r\n"
-	conn.Write([]byte(response))
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	requestLine, err := reader.ReadString('\n')
+	// fmt.Println(requestLine)
+	if err != nil {
+		fmt.Println("Error reading request line: ", err.Error())
+		return
+	}
+
+	parts := strings.Split(strings.TrimSpace(requestLine), " ")
+	if len(parts) == 3 {
+		path := parts[1]
+		if path == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
+	}
 }
